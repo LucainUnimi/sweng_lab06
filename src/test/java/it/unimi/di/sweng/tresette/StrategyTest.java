bf075adabfc59fdd7d900eb92fc35fff87899eef
Luca Dal Mas <luca.dalmas@studenti.unimi.it>
2024-11-11 16:01:04 +0100

    ROSSO: Strategy.RANDOM

---
+++ src/test/java/it/unimi/di/sweng/tresette/StrategyTest.java
@@ -0,0 +1,27 @@
+package it.unimi.di.sweng.tresette;
+
+import it.unimi.di.sweng.tresette.common.Card;
+import it.unimi.di.sweng.tresette.common.Rank;
+import it.unimi.di.sweng.tresette.common.Suit;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Timeout(2)
+public class StrategyTest {
+
+    @Test
+    void RANDOMTest() {
+        Strategy strategy = Strategy.RANDOM;
+        Card choosen = strategy.chooseCard(List.of(
+                Card.get(Rank.ASSO, Suit.BASTONI),
+                Card.get(Rank.DUE, Suit.BASTONI),
+                Card.get(Rank.TRE, Suit.BASTONI),
+                Card.get(Rank.QUATTRO, Suit.BASTONI)
+        ), null);
+        assertThat(choosen).isEqualTo(Card.get(Rank.ASSO, Suit.BASTONI));
+    }
+}
