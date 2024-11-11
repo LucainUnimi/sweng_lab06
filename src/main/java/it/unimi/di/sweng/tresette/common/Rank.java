776a0f8bc37176b91e9726db92a325709f22f8bc
Luca Dal Mas <luca.dalmas@studenti.unimi.it>
2024-11-11 15:32:01 +0100

    VERDE: Rank.tresetteCompare

--- src/main/java/it/unimi/di/sweng/tresette/common/Rank.java
+++ src/main/java/it/unimi/di/sweng/tresette/common/Rank.java
@@ -1,16 +1,33 @@
 package it.unimi.di.sweng.tresette.common;
 
+import java.util.Map;
+
 public enum Rank {
-  //TODO: non potete modificare ordine dei valori,
-  //      ma potete se volete aggiungere attributi e metodi
-  ASSO,
-  DUE,
-  TRE,
-  QUATTRO,
-  CINQUE,
-  SEI,
-  SETTE,
-  FANTE,
-  CAVALLO,
-  RE;
+    ASSO,
+    DUE,
+    TRE,
+    QUATTRO,
+    CINQUE,
+    SEI,
+    SETTE,
+    FANTE,
+    CAVALLO,
+    RE;
+
+    private static final Map<Rank, Integer> TRESETTE_ORDER = Map.of(
+            TRE, 1,
+            DUE, 2,
+            ASSO, 3,
+            RE, 4,
+            CAVALLO, 5,
+            FANTE, 6,
+            SETTE, 7,
+            SEI, 8,
+            CINQUE, 9,
+            QUATTRO, 10
+    );
+
+    public int tresetteCompare(Rank other) {
+        return TRESETTE_ORDER.get(this) - TRESETTE_ORDER.get(other);
+    }
 }
---
+++ src/test/java/it/unimi/di/sweng/tresette/RankTest.java
@@ -0,0 +1,27 @@
+package it.unimi.di.sweng.tresette;
+
+import it.unimi.di.sweng.tresette.common.Rank;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Timeout(2)
+public class RankTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "ASS0, DUE, ASSO",
+            "DUE, TRE, TRE",
+            "RE, CAVALLO, RE",
+            "CAVALLO, FANTE, CAVALLO",
+    })
+    void tresetteCompareTest(String a, String b, String expected) {
+        Rank rankA = Rank.valueOf(a);
+        Rank rankB = Rank.valueOf(b);
+        Rank result = rankA.tresetteCompare(rankB) < 0 ? rankA : rankB;
+        assertThat(result).isEqualTo(Rank.valueOf(expected));
+    }
+}
