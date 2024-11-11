4c4c02276176ca67718818d600d16d4070a4539a
Luca Dal Mas <luca.dalmas@studenti.unimi.it>
2024-11-11 22:29:24 +0100

    VERDE: Player.chooseAnswerCard

--- src/main/java/it/unimi/di/sweng/tresette/Player.java
+++ src/main/java/it/unimi/di/sweng/tresette/Player.java
@@ -43,7 +43,7 @@ public class Player implements Iterable<Card> {
 
     @NotNull
     public Card chooseAnswerCard(@NotNull Card opponent) {
-        return Card.get(Rank.CINQUE, Suit.BASTONI);
+        return answerStrategyChain.chooseCard(cards, opponent);
     }
 
     public int getPoints() {
